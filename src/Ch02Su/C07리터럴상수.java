7a87709fc4a9f586441e48e3df9e2c6e0eef41e3
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-07-11 16:50:03 +0900

    2022-07-11 변수자료형 리터럴상수 자료형정리

---
+++ src/Ch02Su/C07리터럴상수.java
@@ -0,0 +1,34 @@
+package Ch02Su;
+
+public class C07리터럴상수 {
+
+	public static void main(String[] args) {
+		
+		//상수 : 고정된 수
+		//상수 종류
+		//리터럴 상수 : 이름부여 X
+		//심볼릭 상수 : 이름부여 O (final 예약어)
+		
+		//정수 리터럴 기본자료형 : int
+		//실수 리터럴 기본자료형 : double
+		
+		
+		//리터럴 접미사
+		//정수 : l,L(long)
+		//실수 : f,F(float)
+		
+		//예제 1
+//		byte num1 = (byte)129;
+//		long num2 = 123;
+//		double num3 = 123.456;
+//		float num4 = 123.456f;
+//		
+//		System.out.println(num1);
+		
+		//예제 2
+		int num1 = 2147483647;	//int 범위 -2^31 ~ 2^31-1(약21억)
+		long num2 = 4000000000L;	//40억
+		
+	}
+
+}
